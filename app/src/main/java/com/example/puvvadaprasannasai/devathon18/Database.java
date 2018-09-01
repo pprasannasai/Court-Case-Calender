12c4cbf12c51e588fc2eb2d517de6fd63dbeed4c
Puvvada Prasanna Sai <pprasannasai98@gmail.com>
2018-09-02 00:36:42 +0530

    db commit2

--- app/src/main/java/com/example/puvvadaprasannasai/devathon18/Database.java
+++ app/src/main/java/com/example/puvvadaprasannasai/devathon18/Database.java
@@ -1,3 +1,5 @@
+package com.example.puvvadaprasannasai.devathon18;
+
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
@@ -5,7 +7,6 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.widget.EditText;
 
-import com.example.puvvadaprasannasai.devathon18.R;
 
 public class Database extends SQLiteOpenHelper{
 
@@ -20,7 +21,7 @@ public class Database extends SQLiteOpenHelper{
         db.execSQL("Create table case_details(cc_no text, section text, court text, date_filing text, name text, address text, mob_no text)");
         db.execSQL("Create table witness_details(cc_no text, w_no text, w_mob text, w_name text, w_address text)");
         db.execSQL("Create table hearing(cc_no text, hearing_date text)");
-        db.execSQL("Create table proceedings(case_proceeding text, next_hearing text)");
+        db.execSQL("Create table proceedings(cc_no text, case_proceeding text, next_hearing text)");
 
     }
 
@@ -30,17 +31,47 @@ public class Database extends SQLiteOpenHelper{
         db.execSQL("drop table if exists user");
     }
 
-    public void insert_case(SQLiteDatabase db){
+    public void insert_case(Casedetails c){
+        SQLiteDatabase db=this.getWritableDatabase();
+        ContentValues values=new ContentValues();
+        values.put("cc_no", c.cc_no);
+        values.put("section", c.section);
+        values.put("court",c.court);
+        values.put("date_filing",c.date_filing);
+        values.put("name",c.name);
+        values.put("address",c.address);
+        values.put("mob_no",c.mob_no);
+        db.insert("case_details",null,values);
+    }
 
-        EditText cc_no=(EditText) findViewById(R.id.cc_no);
-        EditText section=(EditText) findViewById(R.id.section);
-        EditText court=(EditText) findViewById(R.id.court);
-        EditText date_filing=(EditText) findViewById(R.id.date_filing);
-        EditText name=(EditText) findViewById(R.id.name);
-        EditText address=(EditText) findViewById(R.id.address);
-        EditText mob_no=(EditText) findViewById(R.id.mob_no);
+    public void insert_witness(Witnessdetails w)
+    {
+        SQLiteDatabase db=this.getWritableDatabase();
+        ContentValues values=new ContentValues();
+        values.put("cc_no",w.cc_no);
+        values.put("w_no", w.cc_no);
+        values.put("w_mob",w.w_mob);
+        values.put("w_name",w.w_name);
+        values.put("w_address",w.w_address);
+        db.insert("witness_details",null,values);
+    }
 
-        db.insert()
+    public void insert_hearing(String cc_no,String hearing_date)
+    {
+        SQLiteDatabase db=this.getWritableDatabase();
+        ContentValues values=new ContentValues();
+        values.put("cc_no",cc_no);
+        values.put("hearing_date",hearing_date);
+        db.insert("hearing",null,values);
+    }
 
+    public void insert_proceedings(String cc_no, String case_proceeding, String next_hearing)
+    {
+        SQLiteDatabase db=this.getWritableDatabase();
+        ContentValues values=new ContentValues();
+        values.put("cc_no",cc_no);
+        values.put("next_hearing",next_hearing);
+        values.put("case_proceeding",case_proceeding);
+        db.insert("proceedings",null,values);
     }
 }
